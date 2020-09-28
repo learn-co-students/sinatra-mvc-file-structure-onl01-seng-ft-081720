1a8c4988c206638deae92a3cd552a4e97c0eaaa1
mtrimner <matt.trimner@gmail.com>
2020-09-28 10:26:47 -0700

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,12 @@
+class Dog < ActiveRecord::Base
+
+    # attr_accessor :name, :breed, :age
+    # @@all = []
+    # def initialize(name, breed, age)
+    #     @age = age
+    #     @breed = breed
+    #     @age = age
+    #     @@all < self
+    # end
+
+end
